46d6e685b24072fd6c767d1c5d936e110fc9c649
Henrik N <henrik@nyh.se>
2012-03-16 23:12:12 +0100

    Cleanup.

--- lib/traco/class_methods.rb
+++ lib/traco/class_methods.rb
@@ -4,18 +4,21 @@ module Traco
     def locales_for_column(column)
       column_names.grep(/\A#{column}_([a-z]{2})\z/) {
         $1.to_sym
-      }.sort_by { |x|
-        x == I18n.default_locale ? :"" : x
+      }.sort_by { |locale|
+        if locale == I18n.default_locale
+         :"0"
+        else
+          locale
+        end
       }
     end
 
     def human_attribute_name(attribute, options={})
       default = super(attribute, options.merge(default: ""))
-      if !default.present? && attribute.to_s.match(/\A(\w+)_([a-z]{2})\z/)
+      if default.blank? && attribute.to_s.match(/\A(\w+)_([a-z]{2})\z/)
         column, locale = $1, $2.to_sym
         if translates?(column)
-          locale_name = I18n.t(locale, scope: :"i18n.languages", default: locale.to_s.upcase)
-          return "#{super(column, options)} (#{locale_name})"
+          return "#{super(column, options)} (#{locale_name(locale)})"
         end
       end
       super
@@ -25,6 +28,10 @@ module Traco
 
     def translates?(column)
       @translates_columns.include?(column.to_sym)
+    end
+
+    def locale_name(locale)
+      I18n.t(locale, scope: :"i18n.languages", default: locale.to_s.upcase)
     end
 
   end
