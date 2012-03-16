eefe4437f7d9cdd68e0a934c1403dd7bc90201b3
Henrik N <henrik@nyh.se>
2012-03-16 21:57:04 +0100

    Cleanup.

--- lib/traco/instance_methods.rb
+++ lib/traco/instance_methods.rb
@@ -6,7 +6,6 @@ module Traco
     def read_localized_value(column)
       locales_for_reading_column(column).each do |locale|
         value = public_send("#{column}_#{locale}")
-        value = public_send("#{column}_#{locale}")
         return value if value.present?
       end
       nil
@@ -17,11 +16,11 @@ module Traco
     end
 
     def locales_for_reading_column(column)
-      self.class.locales_for_column(column).sort_by { |x|
-        case x
+      self.class.locales_for_column(column).sort_by { |locale|
+        case locale
         when I18n.locale then :"0"
         when I18n.default_locale then :"1"
-        else x
+        else locale
         end
       }
     end
