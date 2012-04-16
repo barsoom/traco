4915bb791360f5770c2fea5e69d5094a19da3aae
Vinicius Ferriani <viniciusnz@gmail.com>
2012-04-16 07:23:39 -0300

    Making Ruby version 1.8.7 compatible

--- lib/traco/class_methods.rb
+++ lib/traco/class_methods.rb
@@ -18,7 +18,7 @@ module Traco
     end
 
     def human_attribute_name(attribute, options={})
-      default = super(attribute, options.merge(default: ""))
+      default = super(attribute, options.merge(:default => ""))
       if default.blank? && attribute.to_s.match(/\A(\w+)_([a-z]{2})\z/)
         column, locale = $1, $2.to_sym
         if translates?(column)
@@ -35,7 +35,7 @@ module Traco
     end
 
     def locale_name(locale)
-      I18n.t(locale, scope: :"i18n.languages", default: locale.to_s.upcase)
+      I18n.t(locale, :scope => :"i18n.languages", :default => locale.to_s.upcase)
     end
 
   end
