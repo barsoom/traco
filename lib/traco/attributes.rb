026c060095116ba59c640cba570f58d57364e3a4
Nash Bridges <pleasesendmeaspam@gmail.com>
2014-08-14 09:02:34 +0300

    Do not call ActiveRecord methods while including the module.
    This raised an exception in DB rake tasks when DB had not exist yet,
    while AR needed a schema.

--- lib/traco/attributes.rb
+++ lib/traco/attributes.rb
@@ -14,8 +14,6 @@ module Traco
       self.class.ensure_translatable_attributes(base)
       base.translatable_attributes |= @attributes
 
-      self.class.warm_up_activerecord_methods(base)
-
       base.extend ClassMethods
     end
 
@@ -57,15 +55,6 @@ module Traco
 
       base.class_attribute :translatable_attributes
       base.translatable_attributes = []
-    end
-
-    # Force AR to create methods `title_en` etc, which are lazily evaluated,
-    # otherwise we can't safely check `.instance_methods`.
-    def self.warm_up_activerecord_methods(base)
-      random_attribute = base.translatable_attributes.first
-
-      instance = base.new
-      instance.send(Traco.column(random_attribute, I18n.default_locale))
     end
   end
 end
