b99eef292f2ae960f3bcfbfbd62247de5f532604
Henrik Nyh <henrik@nyh.se>
2016-09-30 07:55:38 +0100

    Remove deprecated RSpec option

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,6 @@
 require "i18n"
 
 RSpec.configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
   config.filter_run focus: true
   config.run_all_when_everything_filtered = true
 end
